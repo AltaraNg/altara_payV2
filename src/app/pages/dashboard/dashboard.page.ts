01f410a60dce71aa386f4cdb28943f6e3f7c0293
Damola Oladeji <darmhoo@hotmail.com>
2021-11-05 10:24:25 +0100

    done

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -131,20 +131,7 @@ export class DashboardPage implements OnInit {
 	repaymentDuration: any;
 	repaymentCyclesopt: any;
 	downPaymentRates: any;
-	businessTypes: any;
-	busTypeSlug = {
-		ac_products: 'Altara Credit Products',
-		ap_products: 'Altara Pay Products',
-		ap_cash_loan: 'Altara Pay Cash Loan',
-		ac_cash_loan: 'Altara Credit Cash Loan',
-		'ap_cash_loan-product': 'Altara Pay Cash Loan (Product)',
-		ap_employee_cash_loan: 'Altara Pay Employee Cash Loan',
-		ap_starter_cash_loan: 'Altara Pay Starter Cash Loan',
-		ap_rentals: 'Altara Pay Rentals',
-		'ap_starter_cash_loan-no_collateral':
-			'Altara Pay Starter Cash Loan(No Collateral)',
-		'ap_cash_loan-no_collateral': 'Altara Pay Cash Loan(No Collateral)',
-	};
+	businessTypes: any;	
 	paymentMethods: any;
 	orderTypes: any;
 	banks: any;
